57dbec4a7c31710e92cf3401e88cec0b10208176
Amber Mullen <121705867+armullen@users.noreply.github.com>
2023-09-18 15:47:12 -0500

    reaction timer showing on dom

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <h1>Ninja Reaction Timer</h1>
   <button @click="start" :disabled="isPlaying">Play</button>
-  <Block v-if="isPlaying" :delay="delay" />
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <p v-if="showResults">Reaction Time: {{ score }} ms</p>
 
 </template>
 
@@ -15,7 +16,9 @@ export default {
   data() {
     return {
       isPlaying: false,
-      delay: null
+      delay: null,
+      score: null,
+      showResults: false
     }
   },
   methods: {
@@ -23,6 +26,13 @@ export default {
       this.delay = 2000 + Math.random() * 5000,
       this.isPlaying = true
       console.log(this.delay)
+      this.showResults = false
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
+
     }
   }
 }
